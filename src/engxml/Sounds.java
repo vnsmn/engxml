220fa2ff7a90eb5b009ff97ad87319c186a44522
vns <01_kilimandjaro@ukr.net>
2015-07-31 16:33:10 +0300

    --

--- src/engxml/Sounds.java
+++ src/engxml/Sounds.java
@@ -1,9 +1,11 @@
 package engxml;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.StringReader;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,11 +15,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.sun.jndi.toolkit.url.Uri;
+
 public class Sounds {
 
 	public static void main(String[] args) throws IOException {
+		String vJsn = "elementaryJson";
 		String srcDir = "/home/vns/gtranslator-dictionary/sounds/br/";
-		String trgDir = "/home/vns/workspace/engxml/sound/words/br/";
+		String trgDir = "/home/vns/workspace/engxml/sound/words/br/";				
+		String trgJsonFile = "/home/vns/workspace/engxml/elementary.js";
 		Path phrasesPath = Paths.get(System.getProperty("user.dir"), "elementary.words");
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		Files.copy(phrasesPath, out);
@@ -31,16 +37,18 @@ public class Sounds {
 		String unitTitle = "";
 		String phrases = "";
 		String units = "";
+		String unitPath = "";
 		for (String word : words) {
 			if (word.replaceAll("[ ]+", "").isEmpty()) {
 				continue;
 			}
 			if (word.startsWith("#")) {
 				if (!phrases.isEmpty()) {
-					units += (units.isEmpty() ? "\n" : ",\n") + String.format(f1, unit, unitTitle, phrases.substring(1, phrases.length() - 1));
+					units += (units.isEmpty() ? "\n" : ",\n") + String.format(f1, unit, unitTitle, unitPath, phrases.substring(1, phrases.length() - 1));
 				}
 				unit = word.split("#")[1];
 				unitTitle = word.split("#")[2];
+				unitPath = word.split("#")[3];
 				phrases = "";
 				continue;
 			}
@@ -69,14 +77,18 @@ public class Sounds {
 			String data = String.format(f2, "\"" + word + "\"", "\"" + word + ".mp3\"", sb.toString());			
 			phrases += (phrases.isEmpty() ? "" : "\n") + data +  ",";			
 		}
-		units += (units.isEmpty() ? "\n" : ",\n") + String.format(f1, unit, unitTitle, phrases.substring(1, phrases.length() - 1));
-		System.out.println(units);
+		units += (units.isEmpty() ? "\n" : ",\n") + String.format(f1, unit,  unitTitle, unitPath, phrases.substring(1, phrases.length() - 1));
+		
+		String jsonText = "var " + vJsn + " = {\n" + units + "\n}";
+		System.out.println(jsonText);
+		Files.copy(new ByteArrayInputStream(jsonText.getBytes()), Paths.get(trgJsonFile), StandardCopyOption.REPLACE_EXISTING);
+		
 	}
 	
 static String f1 = 
 "%s : {\n" +
 "	name : \"%s\",\n" +
-"	phrase_path : \"sound/phrases/basic/u1/\",\n" +
+"	phrase_path : \"%s\",\n" +
 "	words_path : \"sound/words/br/\",\n" +
 "	data : [ %s" + " ]\n" +
 "}";

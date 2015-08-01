68198b2af924ff97b2d441f9f73add81bec2a299
vns <01_kilimandjaro@ukr.net>
2015-08-01 23:08:05 +0300

    --

--- elementary.js
+++ elementary.js
@@ -7,158 +7,158 @@ unit2 : {
 	data : [ {
 		phrase : "am_i_late_no_you_re_on_time",
 		source : "am_i_late_no_you_re_on_time.mp3",
-		words : [ "am", "i", "late", "you", "on", "time" ],
-		others : [ ]
+		words : [ "am", "i", "late", "you", "on" ],
+		others : [ "are_they_new.mp3", "how_are_your_parents_are_they_well.mp3" ]
 	},
 	{
 		phrase : "are_these_your_keys_yes_they_are",
 		source : "are_these_your_keys_yes_they_are.mp3",
 		words : [ "are", "these", "your", "they" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "are_they_new",
 		source : "are_they_new.mp3",
-		words : [ "are", "they", "new" ],
-		others : [ ]
+		words : [ "are", "they" ],
+		others : [  ]
 	},
 	{
 		phrase : "are_you_hungry_no_i_m_not_but_i_m_thirsty",
 		source : "are_you_hungry_no_i_m_not_but_i_m_thirsty.mp3",
-		words : [ "are", "you", "i", "m", "not", "but", "thirsty" ],
-		others : [ ]
+		words : [ "are", "you", "i", "m", "not", "but" ],
+		others : [  ]
 	},
 	{
 		phrase : "are_your_parent_s_at_home_no_they_re_out",
 		source : "are_your_parent_s_at_home_no_they_re_out.mp3",
-		words : [ "are", "your", "s", "at", "home", "they", "out" ],
-		others : [ ]
+		words : [ "are", "your", "s", "at", "home", "they" ],
+		others : [  ]
 	},
 	{
 		phrase : "are_your_shoes_new",
 		source : "are_your_shoes_new.mp3",
-		words : [ "are", "your", "new" ],
-		others : [ ]
+		words : [ "are", "your" ],
+		others : [  ]
 	},
 	{
 		phrase : "are_you_tired_yes_i_am",
 		source : "are_you_tired_yes_i_am.mp3",
-		words : [ "are", "you", "i", "am" ],
-		others : [ ]
+		words : [ "are", "you", "i" ],
+		others : [  ]
 	},
 	{
 		phrase : "how_are_your_parents_are_they_well",
 		source : "how_are_your_parents_are_they_well.mp3",
-		words : [ "are", "your", "they", "well" ],
-		others : [ ]
+		words : [ "are", "your", "they" ],
+		others : [  ]
 	},
 	{
 		phrase : "how_much_are_these_postcards_fifty_pence",
 		source : "how_much_are_these_postcards_fifty_pence.mp3",
 		words : [ "much", "are", "these" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "how_old_is_joe_he_s_24",
 		source : "how_old_is_joe_he_s_24.mp3",
 		words : [ "old", "is", "he", "s" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "how_s_your_father",
 		source : "how_s_your_father.mp3",
-		words : [ "s", "your", "father" ],
-		others : [ ]
+		words : [ "s", "your" ],
+		others : [  ]
 	},
 	{
 		phrase : "is_it_cold_in_your_room_yes_a_little",
 		source : "is_it_cold_in_your_room_yes_a_little.mp3",
-		words : [ "is", "it", "in", "your", "a", "little" ],
-		others : [ ]
+		words : [ "is", "it", "in", "your", "a" ],
+		others : [  ]
 	},
 	{
 		phrase : "is_she_at_home",
 		source : "is_she_at_home.mp3",
-		words : [ "is", "she", "at", "home" ],
-		others : [ ]
+		words : [ "is", "she", "at" ],
+		others : [  ]
 	},
 	{
 		phrase : "is_your_friend_english_yes_he_is",
 		source : "is_your_friend_english_yes_he_is.mp3",
 		words : [ "is", "your", "friend", "english", "he" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "is_your_mother_at_home",
 		source : "is_your_mother_at_home.mp3",
-		words : [ "is", "your", "mother", "at", "home" ],
-		others : [ ]
+		words : [ "is", "your", "mother", "at" ],
+		others : [  ]
 	},
 	{
 		phrase : "is_your_mother_at_home_no_she_s_out",
 		source : "is_your_mother_at_home_no_she_s_out.mp3",
-		words : [ "is", "your", "mother", "at", "home", "she", "s", "out" ],
-		others : [ ]
+		words : [ "is", "your", "mother", "at", "home", "she", "s" ],
+		others : [  ]
 	},
 	{
 		phrase : "that_s_my_seat_no_it_isn_t",
 		source : "that_s_my_seat_no_it_isn_t.mp3",
-		words : [ "that", "s", "my", "it", "t" ],
-		others : [ ]
+		words : [ "that", "s", "my", "it" ],
+		others : [  ]
 	},
 	{
 		phrase : "what_colour_is_your_car_it_s_red",
 		source : "what_colour_is_your_car_it_s_red.mp3",
 		words : [ "what", "is", "your", "car", "it", "s" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "what_s_the_time",
 		source : "what_s_the_time.mp3",
-		words : [ "what", "s", "the", "time" ],
-		others : [ ]
+		words : [ "what", "s", "the" ],
+		others : [  ]
 	},
 	{
 		phrase : "where_are_you_from_canada",
 		source : "where_are_you_from_canada.mp3",
 		words : [ "are", "you", "from" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "where_is_your_mother_is_she_at_home",
 		source : "where_is_your_mother_is_she_at_home.mp3",
-		words : [ "is", "your", "mother", "she", "at", "home" ],
-		others : [ ]
+		words : [ "is", "your", "mother", "she", "at" ],
+		others : [  ]
 	},
 	{
 		phrase : "where_s_jill",
 		source : "where_s_jill.mp3",
 		words : [ "s" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "who_s_that_man",
 		source : "who_s_that_man.mp3",
 		words : [ "who", "s", "that" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "why_are_you_angry",
 		source : "why_are_you_angry.mp3",
 		words : [ "are", "you" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "your_shoes_are_nice_are_they_new",
 		source : "your_shoes_are_nice_are_they_new.mp3",
-		words : [ "your", "are", "they", "new" ],
-		others : [ ]
+		words : [ "your", "are", "they" ],
+		others : [  ]
 	},
 	{
 		phrase : "are_these_your_keys_yes_they_are",
 		source : "are_these_your_keys_yes_they_are.mp3",
 		words : [ "are", "these", "your", "they" ],
-		others : [ ]
+		others : [  ]
 	} ]
 },
 unit3 : {
@@ -169,37 +169,37 @@ unit3 : {
 		phrase : "can_you_phone_again_later",
 		source : "can_you_phone_again_later.mp3",
 		words : [ "can", "you", "again" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "she_s_wearing_her_new_hat",
 		source : "she_s_wearing_her_new_hat.mp3",
 		words : [ "she", "s", "wearing", "her", "new" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "the_weather_is_nice_at_the_moment",
 		source : "the_weather_is_nice_at_the_moment.mp3",
 		words : [ "the", "is", "at" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "they_re_playing_in_the_park",
 		source : "they_re_playing_in_the_park.mp3",
 		words : [ "they", "in", "the" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "we_re_having_dinner_now",
 		source : "we_re_having_dinner_now.mp3",
-		words : [ "we", "having", "now" ],
-		others : [ ]
+		words : [ "we", "having" ],
+		others : [  ]
 	},
 	{
 		phrase : "where_are_the_children",
 		source : "where_are_the_children.mp3",
-		words : [ "are", "the", "children" ],
-		others : [ ]
+		words : [ "are", "the" ],
+		others : [  ]
 	} ]
 },
 unit4 : {
@@ -210,103 +210,103 @@ unit4 : {
 		phrase : "are_you_feeling_ok",
 		source : "are_you_feeling_ok.mp3",
 		words : [ "are", "you", "feeling" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "are_your_friends_staying_at_a_hotel",
 		source : "are_your_friends_staying_at_a_hotel.mp3",
-		words : [ "are", "your", "at", "a" ],
-		others : [ ]
+		words : [ "are", "your", "staying", "at", "a" ],
+		others : [  ]
 	},
 	{
 		phrase : "her_parents_live_in_scotland",
 		source : "her_parents_live_in_scotland.mp3",
 		words : [ "her", "live", "in" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "is_it_raining",
 		source : "is_it_raining.mp3",
 		words : [ "is", "it" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "is_it_raining_yes_take_an_umbrella",
 		source : "is_it_raining_yes_take_an_umbrella.mp3",
 		words : [ "is", "it", "take", "an" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "is_paul_working_today",
 		source : "is_paul_working_today.mp3",
 		words : [ "is", "working" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "it_rains_a_lot_in_winter",
 		source : "it_rains_a_lot_in_winter.mp3",
 		words : [ "it", "a", "lot", "in" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "it_s_not_cold",
 		source : "it_s_not_cold.mp3",
 		words : [ "it", "s", "not" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "i_work_in_a_shop_my_brother_works_in_a_bank",
 		source : "i_work_in_a_shop_my_brother_works_in_a_bank.mp3",
 		words : [ "i", "work", "in", "a", "shop", "my", "brother", "works" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "look_there_s_sally",
 		source : "look_there_s_sally.mp3",
 		words : [ "look", "there", "s" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "no_they_aren_t_they_re_staying_with_me",
 		source : "no_they_aren_t_they_re_staying_with_me.mp3",
-		words : [ "they", "t", "with" ],
-		others : [ ]
+		words : [ "they", "t", "staying", "with" ],
+		others : [  ]
 	},
 	{
 		phrase : "what_are_the_children_doing",
 		source : "what_are_the_children_doing.mp3",
-		words : [ "what", "are", "the", "children", "doing" ],
-		others : [ ]
+		words : [ "what", "are", "the", "children" ],
+		others : [  ]
 	},
 	{
 		phrase : "what_s_paul_doing_he_s_reading_the_newspaper",
 		source : "what_s_paul_doing_he_s_reading_the_newspaper.mp3",
 		words : [ "what", "s", "doing", "he", "reading", "the" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "where_s_she_going",
 		source : "where_s_she_going.mp3",
-		words : [ "s", "she", "going" ],
-		others : [ ]
+		words : [ "s", "she" ],
+		others : [  ]
 	},
 	{
 		phrase : "who_are_you_waiting_for_are_you_waiting_for_sue",
 		source : "who_are_you_waiting_for_are_you_waiting_for_sue.mp3",
 		words : [ "who", "are", "you", "for" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "w_hy_are_you_w_earing_a_coat",
 		source : "w_hy_are_you_w_earing_a_coat.mp3",
 		words : [ "are", "you", "a" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "yes_i_m_fine_thank_you",
 		source : "yes_i_m_fine_thank_you.mp3",
-		words : [ "i", "m", "fine", "you" ],
-		others : [ ]
+		words : [ "i", "m", "fine" ],
+		others : [  ]
 	} ]
 },
 unit5 : {
@@ -314,64 +314,64 @@ unit5 : {
 	phrase_path : "sound/phrases/basic/u5/",
 	words_path : "sound/words/br/",
 	data : [ {
-		phrase : "it_costs_a_lot_o_f_money_to_stay_at_luxury_hotels",
-		source : "it_costs_a_lot_o_f_money_to_stay_at_luxury_hotels.mp3",
-		words : [ "it", "a", "lot", "o", "to", "at" ],
-		others : [ ]
+		phrase : "it_costs_a_lot_of_money_to_stay_at_luxury_hotels",
+		source : "it_costs_a_lot_of_money_to_stay_at_luxury_hotels.mp3",
+		words : [ "it", "a", "lot", "of", "money", "to", "stay", "at", "luxury" ],
+		others : [  ]
 	},
 	{
 		phrase : "i_usually_go_to_work_by_car_but_sometimes_i_walk",
 		source : "i_usually_go_to_work_by_car_but_sometimes_i_walk.mp3",
-		words : [ "i", "go", "to", "work", "by", "car", "but", "sometimes", "walk" ],
-		others : [ ]
+		words : [ "i", "go", "to", "work", "by", "car", "but", "sometimes" ],
+		others : [  ]
 	},
 	{
 		phrase : "john_has_a_shower_everyday",
 		source : "john_has_a_shower_everyday.mp3",
 		words : [ "has", "a", "shower" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "julia_never_eats_breakfast",
 		source : "julia_never_eats_breakfast.mp3",
 		words : [  ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "sue_always_arrives_at_work_early",
 		source : "sue_always_arrives_at_work_early.mp3",
-		words : [ "always", "arrives", "at", "work", "early" ],
-		others : [ ]
+		words : [ "always", "arrives", "at", "work" ],
+		others : [  ]
 	},
 	{
 		phrase : "the_earth_goes_round_the_sun",
 		source : "the_earth_goes_round_the_sun.mp3",
 		words : [ "the", "earth", "goes", "round" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "the_shops_open_at_9_o_clock_and_close_at_5_30",
 		source : "the_shops_open_at_9_o_clock_and_close_at_5_30.mp3",
 		words : [ "the", "at", "o", "and" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "tim_works_very_hard_he_starts_at_7_30_and_finishes_at_8_o_clock_in_the_evening",
 		source : "tim_works_very_hard_he_starts_at_7_30_and_finishes_at_8_o_clock_in_the_evening.mp3",
 		words : [ "works", "very", "hard", "he", "starts", "at", "and", "finishes", "o", "in", "the" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "tom_lives_near_us_we_often_see_him",
 		source : "tom_lives_near_us_we_often_see_him.mp3",
 		words : [ "lives", "near", "us", "we", "often", "see" ],
-		others : [ ]
+		others : [  ]
 	},
 	{
 		phrase : "we_do_a_lot_of_different_things_in_our_free_time",
 		source : "we_do_a_lot_of_different_things_in_our_free_time.mp3",
-		words : [ "we", "do", "a", "lot", "of", "different", "in", "our", "free", "time" ],
-		others : [ ]
+		words : [ "we", "do", "a", "lot", "of", "different", "in", "our", "free" ],
+		others : [  ]
 	} ]
 }
 }
--- src/engxml/Sounds.java
+++ src/engxml/Sounds.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -22,17 +23,21 @@ public class Sounds {
 	public static void main(String[] args) throws IOException {
 		String vJsn = "elementaryJson";
 		String srcDir = "/home/vns/gtranslator-dictionary/sounds/br/";
-		String trgDir = "/home/vns/workspace/engxml/sound/words/br/";				
+		String trgDir = "/home/vns/workspace/engxml/sound/words/br/";
 		String trgJsonFile = "/home/vns/workspace/engxml/elementary.js";
-		Path phrasesPath = Paths.get(System.getProperty("user.dir"), "elementary.words");
+		Path phrasesPath = Paths.get(System.getProperty("user.dir"),
+				"elementary.words");
+		// System.out.println(phrasesPath.toFile().getAbsolutePath());
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		Files.copy(phrasesPath, out);
-		BufferedReader reader = new BufferedReader(new StringReader(new String(out.toByteArray())));		
+		BufferedReader reader = new BufferedReader(new StringReader(new String(
+				out.toByteArray())));
 		List<String> words = new ArrayList<>();
 		String s;
 		while ((s = reader.readLine()) != null) {
-			words.add(s.split(".mp3")[0]);
+			words.add(s);
 		}
+		// System.out.println(Arrays.toString(words.toArray()));
 		String unit = "";
 		String unitTitle = "";
 		String phrases = "";
@@ -44,7 +49,9 @@ public class Sounds {
 			}
 			if (word.startsWith("#")) {
 				if (!phrases.isEmpty()) {
-					units += (units.isEmpty() ? "\n" : ",\n") + String.format(f1, unit, unitTitle, unitPath, phrases.substring(1, phrases.length() - 1));
+					units += (units.isEmpty() ? "\n" : ",\n")
+							+ String.format(f1, unit, unitTitle, unitPath,
+									phrases.substring(1, phrases.length() - 1));
 				}
 				unit = word.split("#")[1];
 				unitTitle = word.split("#")[2];
@@ -52,10 +59,28 @@ public class Sounds {
 				phrases = "";
 				continue;
 			}
+			String[] ws = word.split("#");
 			StringBuilder sb = new StringBuilder();
 			Set<String> duplicates = new HashSet<String>();
-			for (String w : word.split("[ ,;_]")) {
-				String normal = w.trim();				
+			for (int i = 1; i < ws.length; i++) {
+				String normal = ws[i].trim();
+				if (!normal.isEmpty() && !duplicates.contains(normal)) {
+					duplicates.add(normal);
+					if (sb.length() > 0) {
+						sb.append(", ");
+					}
+					sb.append("\"");
+					sb.append(normal);
+					sb.append("\"");
+				}
+			}
+			String parts = sb.toString();
+
+			String phrase = ws[0];
+			sb = new StringBuilder();
+			duplicates = new HashSet<String>();
+			for (String w : phrase.split("[ ,;_]")) {
+				String normal = w.trim();
 				if (!normal.isEmpty() && !duplicates.contains(normal)) {
 					duplicates.add(normal);
 					Path src = Paths.get(srcDir, normal + ".mp3");
@@ -74,29 +99,25 @@ public class Sounds {
 					}
 				}
 			}
-			String data = String.format(f2, "\"" + word + "\"", "\"" + word + ".mp3\"", sb.toString());			
-			phrases += (phrases.isEmpty() ? "" : "\n") + data +  ",";			
+			String data = String.format(f2, "\"" + phrase.replace(".mp3", "")
+					+ "\"", "\"" + phrase + "\"", sb.toString(), parts);
+			phrases += (phrases.isEmpty() ? "" : "\n") + data + ",";
 		}
-		units += (units.isEmpty() ? "\n" : ",\n") + String.format(f1, unit,  unitTitle, unitPath, phrases.substring(1, phrases.length() - 1));
-		
+		units += (units.isEmpty() ? "\n" : ",\n")
+				+ String.format(f1, unit, unitTitle, unitPath,
+						phrases.substring(1, phrases.length() - 1));
+
 		String jsonText = "var " + vJsn + " = {\n" + units + "\n}";
 		System.out.println(jsonText);
-		Files.copy(new ByteArrayInputStream(jsonText.getBytes()), Paths.get(trgJsonFile), StandardCopyOption.REPLACE_EXISTING);
-		
+		Files.copy(new ByteArrayInputStream(jsonText.getBytes()),
+				Paths.get(trgJsonFile), StandardCopyOption.REPLACE_EXISTING);
+
 	}
-	
-static String f1 = 
-"%s : {\n" +
-"	name : \"%s\",\n" +
-"	phrase_path : \"%s\",\n" +
-"	words_path : \"sound/words/br/\",\n" +
-"	data : [ %s" + " ]\n" +
-"}";
-static String f2 =
-"\t{\n" +
-"\t\tphrase : %s,\n" +
-"\t\tsource : %s,\n" +
-"\t\twords : [ %s ],\n" +
-"\t\tothers : [ ]\n" +
-"\t}";
+
+	static String f1 = "%s : {\n" + "	name : \"%s\",\n"
+			+ "	phrase_path : \"%s\",\n"
+			+ "	words_path : \"sound/words/br/\",\n" + "	data : [ %s" + " ]\n"
+			+ "}";
+	static String f2 = "\t{\n" + "\t\tphrase : %s,\n" + "\t\tsource : %s,\n"
+			+ "\t\twords : [ %s ],\n" + "\t\tothers : [ %s ]\n" + "\t}";
 }
